13ee23142b967cc1732950f8d655028376bfe192
Vu Ngoc Hong Phuc <phucvnh@gmail.com>
2018-12-14 13:43:54 +0700

    add redux dev tool

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 
 import placeReducer from './reducer/places';
 
@@ -6,8 +6,14 @@ const rootReducer = combineReducers({
   places: placeReducer
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+}
+
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, composeEnhancers());
 };
 
 export default configureStore;
