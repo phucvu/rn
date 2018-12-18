f32d3f77946a847f349a3acc1f5d178535332ca5
Vu Ngoc Hong Phuc <phucvnh@gmail.com>
2018-12-18 13:09:15 +0700

    add react-native-navigation 2

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Modal, View, Image, Text, Button } from 'react-native';
+import { StyleSheet, Modal, View, Image, Text, Button, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const placeDetail = props => {
   let modalContent = null; 
@@ -22,6 +24,11 @@ const placeDetail = props => {
       <View style={styles.modalContainer}>
         {modalContent}
         <View>
+          <TouchableOpacity onPress={props.onItemDeleted}>
+            <View style={styles.deleteButton}>
+              <Icon size={30} name="ios-trash" color="red"> </Icon>
+            </View>
+          </TouchableOpacity>
           <Button title="Delete" color="red" onPress={props.onItemDeleted} />
           <Button title="Close" onPress={props.onModalClosed} />
         </View>
@@ -42,6 +49,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 28
+  },
+  deleteButton: {
+    alignItems: "center"
   }
 });
 
