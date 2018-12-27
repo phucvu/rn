b0c8320359ca635456a05d430d5fcf3cb4d3d7d4
Vu Ngoc Hong Phuc <phucvnh@gmail.com>
2018-12-27 11:06:54 +0700

    add background image

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
 import { connect } from 'react-redux';
+
 import startMainTabs from '../MainTabs/startMainTabs';
+import InputDefault from '../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
+import backgroundImage from '../../assets/background.jpg';
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 
 class AuthScreen extends Component {
-  
   loginHandler = () => {
     startMainTabs()
   }
   
   render () {
     return (
-      <View>
-        <Text> Auth Screen </Text>
-        <Button title="Login" onPress={this.loginHandler} />
-      </View>
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText> Please Log In </HeadingText>
+          </MainText>
+          <ButtonWithBackground color="#29aaf4" onPress={() => alert('hele')}>
+            Switch to Login
+          </ButtonWithBackground>
+          <View style={styles.inputContainer}>
+            <InputDefault placeholder="Your E-Mail Address" />
+            <InputDefault placeholder="Password" />
+            <InputDefault placeholder="Confirm password" />
+          </View>
+          <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>
+            Submit
+          </ButtonWithBackground>
+        </View>
+      </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%"
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  textHeading: {
+    fontSize: 28,
+    fontWeight: "bold"
+  },
+  input: {
+    backgroundColor: "#eee",
+    borderColor: "#bbb"
+  }
+});
 
 export default connect(null, {})(AuthScreen);
