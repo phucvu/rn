777d76828d4a305db1aa694cd18aed2512962d4a
Vu Ngoc Hong Phuc <phucvnh@gmail.com>
2019-01-15 14:17:47 +0700

    add upload file to firebase

--- src/store/action/places.js
+++ src/store/action/places.js
@@ -1,11 +1,32 @@
 import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
+  return dispatch => {
+    fetch("https://us-central1-awesome-places-f04c4.cloudfunctions.net/storeImage", {
+      method: "POST", 
+      body: JSON.stringify({
+        image: image.base64
+      })
+    })
+    .catch(err => console.log(err))
+    .then(res => res.json())
+    .then(parsedRes => {
+      const placeData = {
+        name: placeName,
+        location: location,
+        image: parsedRes.imageUrl
+      };
+
+      return fetch("https://awesome-places-1546831277287.firebaseio.com/places.json", {
+        method: "POST",
+        body: JSON.stringify(placeData)
+      })
+    })
+    .catch(err => console.log(err))
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes);
+    });
   }
 };
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import placeReducer from './reducer/places';
 
@@ -10,10 +11,10 @@ let composeEnhancers = compose;
 
 if (__DEV__) {
   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-}
+};
 
 const configureStore = () => {
-  return createStore(rootReducer, composeEnhancers());
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 };
 
 export default configureStore;
